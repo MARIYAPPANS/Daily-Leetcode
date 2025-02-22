a7b884748b4798e45c01a4c61d561a0f2dd64636
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2025-02-22 01:58:07 -0800

    Updated

--- Medium_Problems/2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.py
+++ Medium_Problems/2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.py
@@ -1,19 +1,11 @@
-from typing import List
-
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        ds, max_s = {}, -1
+        new = {}
+        max_sum = -1
+        for num in nums:
+            digit_sum = sum(int(j) for j in str(num))
+            if digit_sum in new:
+                max_sum = max(max_sum, new[digit_sum] + num)
+            new[digit_sum] = max(new.get(digit_sum, 0), num)
         
-        for n in nums:
-            s, temp = 0, n
-            while temp:  # Compute digit sum without string conversion
-                s += temp % 10
-                temp //= 10
-            
-            if s in ds:
-                max_s = max_s if max_s > ds[s] + n else ds[s] + n
-                ds[s] = ds[s] if ds[s] > n else n
-            else:
-                ds[s] = n
-        
-        return max_s
+        return max_sum
