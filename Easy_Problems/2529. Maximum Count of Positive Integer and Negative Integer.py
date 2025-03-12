8c6736946d2cbd9cee72966d86a73fa95a19d43f
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2025-03-12 11:47:28 +0530

    Create 2529. Maximum Count of Positive Integer and Negative Integer.py

---
+++ Easy_Problems/2529. Maximum Count of Positive Integer and Negative Integer.py
@@ -0,0 +1,8 @@
+from typing import List
+import bisect
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        neg = bisect.bisect_left(nums, 0)
+        pos = len(nums) - bisect.bisect_right(nums, 0)
+        return pos if pos > neg else neg
